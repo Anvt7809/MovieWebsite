d8780c4bcbb17c48b91241630c64848bb4aebb9e
Tú Anh <64167129+Anvt7809@users.noreply.github.com>
2020-11-08 19:46:06 +0700

    Add files via upload

---
+++ Models/SeedData.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Data;
+using System;
+using System.Linq;
+
+namespace RazorPagesMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                // looking for any movies
+                if (context.Movie.Any())
+                {
+                    return; //DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "How I meet your mother",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        Rating = "R"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Harry Potter and The Half-Prince",
+                        ReleaseDate = DateTime.Parse("2007-2-12"),
+                        Genre = "Adventure",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Black Widow",
+                        ReleaseDate = DateTime.Parse("2021-4-21"),
+                        Genre = "Action",
+                        Price = 8.99M,
+                        Rating = "R"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
